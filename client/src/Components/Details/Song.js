0024d163f20abb8ef74ec8d66310add02b8efdcd
Deo G <66809588+921Mdas@users.noreply.github.com>
2022-07-25 17:58:16 -0400

    songplayer

--- client/src/Components/Details/Song.js
+++ client/src/Components/Details/Song.js
@@ -1,13 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 // components
 import SongCtrl from "./SongCtrl";
 import Loader from "../UtilComponent/Loader";
 
 const Song = ({ state, dispatch, COMMANDS, playlist, name }) => {
+  const [play, setPlay] = useState(false);
+  const nowPlaying = useRef(null);
+
+  const starTrack = () => {
+    setPlay(!play);
+  };
+
   if (playlist?.length > 0) {
     return playlist.map(track => {
-      return <SongCtrl track={track} key={track._id} />;
+      return (
+        <SongCtrl
+          track={track}
+          key={track._id}
+          starTrack={starTrack}
+          nowPlaying={nowPlaying}
+        />
+      );
     });
   } else {
     return <Loader />;
--- client/src/Components/Details/SongCtrl.js
+++ client/src/Components/Details/SongCtrl.js
@@ -2,12 +2,17 @@ import React, { useState } from "react";
 
 import Actionbar from "./Actionbar";
 
-const SongCtrl = ({ track }) => {
+const SongCtrl = ({ track, starTrack, nowPlaying }) => {
   return (
     <div className="detail_song">
       <div className="detail_info">
         <h5>{track.title}</h5>
-        <audio src={track.track} controls />
+        <audio
+          src={track.track}
+          ref={nowPlaying}
+          onPlay={() => starTrack()}
+          controls
+        />
       </div>
       <div className="track_actions">
         <Actionbar />
