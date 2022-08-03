8ec659f0b77e053a5d3c7b3fa4a1d89c550cad04
Deo G <66809588+921Mdas@users.noreply.github.com>
2022-08-02 20:02:11 -0400

    done upload nodejs

--- client/src/Components/Details/UploadForm.js
+++ client/src/Components/Details/UploadForm.js
@@ -9,21 +9,24 @@ import { BiEqualizer } from "react-icons/bi";
 import { HiUpload } from "react-icons/hi";
 import { FiDisc } from "react-icons/fi";
 import { RiMusic2Line } from "react-icons/ri";
-import { BsSoundwave } from "react-icons/bs";
+import { BsSoundwave, BsCloudCheckFill } from "react-icons/bs";
 
 const UploadForm = ({ sendMusic }) => {
   const FileUpload = useRef(null);
   const [formState, setFormState] = useState({
     file: "",
   });
-
+  const [isUploaded, setIsUploaded] = useState(false);
+  const fileName = formState?.file?.name?.split(".")[0];
   const handleChange = e => {
     e.preventDefault();
     const targetName = e.target.name;
     const targetValue = e.target.files[0];
+    console.log("the file changed");
 
     if (targetName && targetValue) {
       setFormState({ ...formState, [targetName]: targetValue });
+      setIsUploaded(true);
     } else {
       console.log("form incomplete");
     }
@@ -33,17 +36,19 @@ const UploadForm = ({ sendMusic }) => {
     e.preventDefault();
     const data = new FormData();
     data.append("file", formState.file);
-    const fileName = formState.file.name.split(".")[0];
     sendMusic(data, fileName);
+    setIsUploaded(false);
   };
 
   // form that uploads audio files to aws S3 bucket
-
   return (
     <>
       <Form className="uploadform" action="#">
         <Form.Label htmlFor="file" className="upload_label">
-          <p>Click to Upload</p>
+          <p>
+            Click to Upload{" "}
+            <span>{fileName && isUploaded ? fileName : ""}</span>
+          </p>{" "}
           <BsSoundwave className="upload_icon" />
         </Form.Label>
         <Form.Control
@@ -55,8 +60,16 @@ const UploadForm = ({ sendMusic }) => {
           className="dissapear"
         />
 
-        <Button className="uploadBtn" onClick={e => handleSubmit(e)}>
-          <HiUpload className="upload_icon_btn" />
+        <Button
+          className={`uploadBtn ${isUploaded ? "" : "disabled_btn"}`}
+          disabled={isUploaded ? false : true}
+          onClick={e => handleSubmit(e)}
+        >
+          {isUploaded ? (
+            <BsCloudCheckFill className="upload_success" color="white" />
+          ) : (
+            <HiUpload className="upload_icon_btn" />
+          )}
         </Button>
       </Form>
     </>
